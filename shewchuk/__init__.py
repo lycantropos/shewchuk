a0fab7878f649b216a509e816da19c69a10fbcb6
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-14 19:16:06 +0300

    Complete 'Expansion' class

--- shewchuk/__init__.py
+++ shewchuk/__init__.py
@@ -3,9 +3,26 @@
 __version__ = '0.0.0'
 
 try:
-    from _shewchuk import Quadruple
+    from _shewchuk import (Expansion,
+                           Quadruple)
 except ImportError:
-    from typing import Tuple
+    from typing import (Sequence as _Sequence,
+                        Tuple as _Tuple)
+
+
+    class Expansion:
+        __slots__ = '_components',
+
+        def __new__(cls, *components: float) -> 'Expansion':
+            self = super().__new__(cls)
+            if len(components) > 1:
+                components = tuple(_compress_components(components))
+            self._components = components or (0.,)
+            return self
+
+        def __repr__(self) -> str:
+            return (type(self).__qualname__
+                    + '({})'.format(', '.join(map(str, self._components))))
 
 
     class Quadruple:
@@ -23,7 +40,30 @@ except ImportError:
                         else '({})').format(self._head, self._tail)))
 
 
-    def _two_add(left: float, right: float) -> Tuple[float, float]:
+    def _compress_components(components: _Sequence[float]) -> _Sequence[float]:
+        bottom = len(components) - 1
+        cursor = components[bottom]
+        result = [None] * len(components)
+        for index in range(bottom - 1, -1, -1):
+            head, tail = _two_add(cursor, components[index])
+            if tail:
+                result[bottom] = head
+                bottom -= 1
+                cursor = tail
+            else:
+                cursor = head
+        top = 0
+        for index in range(bottom + 1, len(result)):
+            head, tail = _two_add(result[index], cursor)
+            if tail:
+                result[top] = tail
+                top += 1
+            cursor = head
+        result[top] = cursor
+        return result[:top + 1]
+
+
+    def _two_add(left: float, right: float) -> _Tuple[float, float]:
         head = left + right
         right_virtual = head - left
         left_virtual = head - right_virtual
