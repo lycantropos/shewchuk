5f42f7a617c989f53ed4e2216fc38c2521cc4a4b
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-15 13:04:13 +0300

    Add test for 'Expansion.__bool__' magic method

---
+++ tests/expansion_tests/test_bool.py
@@ -0,0 +1,24 @@
+import sys
+
+from hypothesis import given
+
+from shewchuk import Expansion
+from tests.utils import (equivalence,
+                         skip_reference_counter_test)
+from . import strategies
+
+
+@given(strategies.expansions)
+def test_properties(expansion: Expansion) -> None:
+    assert equivalence(bool(expansion), bool(float(expansion)))
+
+
+@skip_reference_counter_test
+@given(strategies.expansions)
+def test_reference_counter(expansion: Expansion) -> None:
+    expansion_refcount_before = sys.getrefcount(expansion)
+
+    result = bool(expansion)
+
+    expansion_refcount_after = sys.getrefcount(expansion)
+    assert expansion_refcount_after == expansion_refcount_before
