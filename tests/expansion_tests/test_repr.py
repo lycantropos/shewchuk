a5b5dee97a1a73e30d6ca6b6e1ddb8d235e5206c
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-15 23:19:18 +0300

    Add test for 'Expansion.__repr__' magic method

---
+++ tests/expansion_tests/test_repr.py
@@ -0,0 +1,33 @@
+import sys
+
+from hypothesis import given
+
+import shewchuk
+from shewchuk import Expansion
+from tests.utils import skip_reference_counter_test
+from . import strategies
+
+
+@given(strategies.expansions)
+def test_basic(expansion: Expansion) -> None:
+    result = repr(expansion)
+
+    assert result.startswith(Expansion.__qualname__)
+
+
+@given(strategies.expansions)
+def test_round_trip(expansion: Expansion) -> None:
+    result = repr(expansion)
+
+    assert eval(result, vars(shewchuk)) == expansion
+
+
+@skip_reference_counter_test
+@given(strategies.expansions)
+def test_reference_counter(expansion: Expansion) -> None:
+    expansion_refcount_before = sys.getrefcount(expansion)
+
+    result = repr(expansion)
+
+    expansion_refcount_after = sys.getrefcount(expansion)
+    assert expansion_refcount_after == expansion_refcount_before
