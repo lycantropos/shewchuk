00378816ee85b1f38121e2b32541f14c6ab99c28
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-16 01:36:00 +0300

    Complete constructor of 'Expansion' class

--- src/shewchuk.c
+++ src/shewchuk.c
@@ -111,16 +111,18 @@ size_t compress_components_single(size_t size, double *components) {
 
 size_t compress_components(size_t size, double *components) {
   const size_t original_size = size;
-  double *next_components = calloc(size, sizeof(double));
-  memcpy(next_components, components, size * sizeof(double));
+  double *next_components = PyMem_RawCalloc(size, sizeof(double));
+  for (size_t index = 0; index < size; ++index)
+    next_components[index] = components[index];
   for (size_t step = 0; step < original_size; ++step) {
     const size_t next_size = compress_components_single(size, next_components);
     if (are_components_equal(next_size, next_components, size, components))
       break;
     size = next_size;
-    memcpy(components, next_components, size * sizeof(double));
+    for (size_t index = 0; index < size; ++index)
+      components[index] = next_components[index];
   }
-  free(next_components);
+  PyMem_RawFree(next_components);
   return size;
 }
 
