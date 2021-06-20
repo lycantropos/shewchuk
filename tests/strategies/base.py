4745f651a2325b3b221c0c360fe7a322305795c0
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-21 05:19:56 +0700

    Complete strategies

--- tests/strategies/base.py
+++ tests/strategies/base.py
@@ -1,6 +1,6 @@
 from hypothesis import strategies
 
-MAX_VALUE = 10 ** 100
+MAX_VALUE = 10 ** 50
 finite_floats = strategies.floats(-float(MAX_VALUE), float(MAX_VALUE),
                                   allow_infinity=False,
                                   allow_nan=False)
