f9dafe088fae34bf10fb5f1b1cd3c78b0dd1af15
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-21 20:13:36 +0700

    Complete test for 'Expansion.__mul__' magic method

--- tests/expansion_tests/test_mul.py
+++ tests/expansion_tests/test_mul.py
@@ -1,5 +1,6 @@
 import sys
 from numbers import Real
+from typing import Union
 
 from hypothesis import given
 
@@ -8,15 +9,20 @@ from tests.utils import skip_reference_counter_test
 from . import strategies
 
 
-@given(strategies.expansions, strategies.reals)
-def test_basic(first: Expansion, second: Real) -> None:
+@given(strategies.expansions, strategies.reals_or_expansions)
+def test_basic(first: Expansion, second: Union[Real, Expansion]) -> None:
     result = first * second
 
     assert isinstance(result, Expansion)
 
 
-@given(strategies.expansions, strategies.zero_reals)
-def test_absorbing_element(first: Expansion, second: Real) -> None:
+@given(strategies.expansions, strategies.reals)
+def test_real_argument(first: Expansion, second: Real) -> None:
+    assert first * second == first * Expansion(second)
+
+
+@given(strategies.expansions, strategies.zero_expansions)
+def test_absorbing_element(first: Expansion, second: Expansion) -> None:
     assert first * second == second == second * first
 
 
