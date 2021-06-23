4e5cd7b0ead5c754b012507280c6282e216879e4
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-23 18:37:18 +0700

    Add test for 'Expansion.__ceil__' magic method

---
+++ tests/expansion_tests/test_ceil.py
@@ -0,0 +1,20 @@
+import math
+
+from hypothesis import given
+
+from shewchuk import Expansion
+from . import strategies
+
+
+@given(strategies.expansions)
+def test_basic(expansion: Expansion) -> None:
+    result = math.ceil(expansion)
+
+    assert isinstance(result, int)
+
+
+@given(strategies.expansions)
+def test_value(expansion: Expansion) -> None:
+    result = math.ceil(expansion)
+
+    assert result >= expansion
