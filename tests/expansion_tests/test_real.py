d041920ec83df481866493463c6d343cd57f1a43
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-28 00:15:58 +0300

    Add test for 'Expansion.real' property

---
+++ tests/expansion_tests/test_real.py
@@ -0,0 +1,32 @@
+import sys
+
+from hypothesis import given
+
+from shewchuk import Expansion
+from tests.utils import skip_reference_counter_test
+from . import strategies
+
+
+@given(strategies.expansions)
+def test_basic(expansion: Expansion) -> None:
+    result = expansion.real
+
+    assert isinstance(result, Expansion)
+
+
+@given(strategies.expansions)
+def test_identity(expansion: Expansion) -> None:
+    result = expansion.real
+
+    assert result == expansion
+
+
+@skip_reference_counter_test
+@given(strategies.expansions)
+def test_reference_counter(expansion: Expansion) -> None:
+    expansion_refcount_before = sys.getrefcount(expansion)
+
+    result = expansion.real
+
+    expansion_refcount_after = sys.getrefcount(expansion)
+    assert expansion_refcount_after == expansion_refcount_before + 1
