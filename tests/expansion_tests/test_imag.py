d9deec0a6b00e367e311544171733ef3bce02522
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-28 00:17:16 +0300

    Add test for 'Expansion.imag' property

---
+++ tests/expansion_tests/test_imag.py
@@ -0,0 +1,33 @@
+import sys
+from numbers import Real
+
+from hypothesis import given
+
+from shewchuk import Expansion
+from tests.utils import skip_reference_counter_test
+from . import strategies
+
+
+@given(strategies.expansions)
+def test_basic(expansion: Expansion) -> None:
+    result = expansion.imag
+
+    assert isinstance(result, Real)
+
+
+@given(strategies.expansions)
+def test_zeroness(expansion: Expansion) -> None:
+    result = expansion.imag
+
+    assert not result
+
+
+@skip_reference_counter_test
+@given(strategies.expansions)
+def test_reference_counter(expansion: Expansion) -> None:
+    expansion_refcount_before = sys.getrefcount(expansion)
+
+    result = expansion.imag
+
+    expansion_refcount_after = sys.getrefcount(expansion)
+    assert expansion_refcount_after == expansion_refcount_before
