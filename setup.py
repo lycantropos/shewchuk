0032092a506b5365ce0303d397076f9f2a7c75d3
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-08 00:18:04 +0700

    Complete 'setup.py' script

--- setup.py
+++ setup.py
@@ -38,32 +38,10 @@ parameters = dict(
         python_requires='>=3.6')
 if platform.python_implementation() == 'CPython':
     from glob import glob
-    from typing import Any
 
-    from setuptools import (Command,
-                            Extension)
-    from setuptools.command.build_ext import build_ext
-    from setuptools.command.develop import develop
+    from setuptools import Extension
 
-
-    class Develop(develop):
-        def reinitialize_command(self,
-                                 name: str,
-                                 reinit_subcommands: int = 0,
-                                 **kwargs: Any) -> Command:
-            if name == build_ext.__name__:
-                kwargs.setdefault('debug', 1)
-            result = super().reinitialize_command(name, reinit_subcommands,
-                                                  **kwargs)
-            if name == build_ext.__name__:
-                result.ensure_finalized()
-                for extension in result.extensions:
-                    extension.undef_macros.append(('NDEBUG',))
-            return result
-
-
-    parameters.update(cmdclass={develop.__name__: Develop},
-                      ext_modules=[Extension('_' + shewchuk.__name__,
+    parameters.update(ext_modules=[Extension('_' + shewchuk.__name__,
                                              glob('src/*.c'))],
                       zip_safe=False)
 setup(**parameters)
