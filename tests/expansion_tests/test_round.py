7fa8554d944e638e92dd589d86654f51fc7351a5
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-07 23:33:04 +0700

    Complete test of 'Expansion.__round__' magic method

--- tests/expansion_tests/test_round.py
+++ tests/expansion_tests/test_round.py
@@ -3,6 +3,7 @@ from typing import Optional
 from hypothesis import given
 
 from shewchuk import Expansion
+from tests.utils import to_sign
 from . import strategies
 
 
@@ -10,11 +11,16 @@ from . import strategies
 def test_basic(expansion: Expansion, precision: Optional[int]) -> None:
     result = round(expansion, precision)
 
-    assert isinstance(result, int if precision is None else float)
+    assert isinstance(result, int if precision is None else Expansion)
 
 
-@given(strategies.expansions, strategies.precisions)
-def test_value(expansion: Expansion, precision: Optional[int]) -> None:
-    result = round(expansion, precision)
+@given(strategies.expansions)
+def test_value(expansion: Expansion) -> None:
+    result = round(expansion)
 
-    assert result == round(float(expansion), precision)
+    truncated_expansion = int(expansion)
+    distance = abs(truncated_expansion - expansion)
+    assert (result == truncated_expansion + to_sign(expansion)
+            if (distance > 0.5
+                or distance == 0.5 and truncated_expansion % 2 == 1)
+            else result == truncated_expansion)
