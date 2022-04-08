599d6de26e031086a1af3610a8f02cbdf7156748
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-08 11:18:26 +0700

    Complete test of 'Expansion.__floordiv__' magic method

--- tests/expansion_tests/test_floordiv.py
+++ tests/expansion_tests/test_floordiv.py
@@ -13,36 +13,44 @@ from . import strategies
 
 
 @given(strategies.expansions, strategies.non_zero_reals_or_expansions)
-def test_basic(first: Expansion, second: Union[Real, Expansion]) -> None:
-    result = first // second
+def test_basic(dividend: Expansion, divisor: Union[Real, Expansion]) -> None:
+    result = dividend // divisor
 
     assert isinstance(result, Expansion)
     assert math.isfinite(result)
 
 
 @given(strategies.expansions, strategies.non_zero_reals_or_expansions)
-def test_value(first: Expansion, second: Union[Real, Expansion]) -> None:
-    result = first // second
+def test_value(dividend: Expansion, divisor: Union[Real, Expansion]) -> None:
+    result = dividend // divisor
 
-    assert implication(not first, not result)
+    assert implication(not dividend, not result)
+
+
+@given(strategies.expansions, strategies.non_zero_reals_or_expansions)
+def test_connection_with_mod(dividend: Expansion,
+                             divisor: Union[Real, Expansion]) -> None:
+    result = dividend // divisor
+
+    assert result * divisor + (dividend % divisor) == dividend
 
 
 @skip_reference_counter_test
 @given(strategies.expansions, strategies.non_zero_expansions)
-def test_reference_counter(first: Expansion, second: Expansion) -> None:
-    first_refcount_before = sys.getrefcount(first)
-    second_refcount_before = sys.getrefcount(second)
+def test_reference_counter(dividend: Expansion, divisor: Expansion) -> None:
+    first_refcount_before = sys.getrefcount(dividend)
+    second_refcount_before = sys.getrefcount(divisor)
 
-    result = first // second
+    result = dividend // divisor
 
-    first_refcount_after = sys.getrefcount(first)
-    second_refcount_after = sys.getrefcount(second)
+    first_refcount_after = sys.getrefcount(dividend)
+    second_refcount_after = sys.getrefcount(divisor)
     assert first_refcount_after == first_refcount_before
     assert second_refcount_after == second_refcount_before
 
 
 @given(strategies.expansions, strategies.zero_reals_or_expansions)
-def test_zero_divisor(first: Expansion,
-                      second: Union[Real, Expansion]) -> None:
+def test_zero_divisor(dividend: Expansion, divisor: Union[Real, Expansion]
+                      ) -> None:
     with pytest.raises(ZeroDivisionError):
-        first // second
+        dividend // divisor
