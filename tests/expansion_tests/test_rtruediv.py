e8abb318d8df70d4df8fc17b3966275f7344d70d
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-14 14:56:56 +0700

    Complete test of 'Expansion.__rtruediv__' magic method

--- tests/expansion_tests/test_rtruediv.py
+++ tests/expansion_tests/test_rtruediv.py
@@ -6,13 +6,6 @@ from tests.utils import LeftOperand
 from . import strategies
 
 
-@given(strategies.reals, strategies.non_zero_expansions)
-def test_alternatives(first: LeftOperand, second: Expansion) -> None:
-    result = first / second
-
-    assert result == first / float(second)
-
-
 @given(strategies.reals, strategies.zero_expansions)
 def test_zero_divisor(first: LeftOperand, second: Expansion) -> None:
     with pytest.raises(ZeroDivisionError):
