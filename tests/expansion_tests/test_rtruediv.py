5f80d6bec36cc24d789cb81ed94d82c94572c0fb
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-14 15:33:19 +0700

    Complete test of 'Expansion.__rtruediv__' magic method

--- tests/expansion_tests/test_rtruediv.py
+++ tests/expansion_tests/test_rtruediv.py
@@ -2,8 +2,17 @@ import pytest
 from hypothesis import given
 
 from shewchuk import Expansion
-from tests.utils import LeftOperand
+from tests.utils import (LeftOperand,
+                         is_expansion_valid)
 from . import strategies
+
+
+@given(strategies.reals, strategies.non_zero_expansions)
+def test_alternatives(first: LeftOperand, second: Expansion) -> None:
+    result = first / second
+
+    assert isinstance(result, Expansion)
+    assert is_expansion_valid(result)
 
 
 @given(strategies.reals, strategies.zero_expansions)
