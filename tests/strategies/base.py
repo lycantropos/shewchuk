bf700b26ec762f3f78b6acf2a5fb4619dd1148ed
Azat Ibrakov <azatibrakov@gmail.com>
2023-01-05 03:03:17 +0100

    Fix strategies

--- tests/strategies/base.py
+++ tests/strategies/base.py
@@ -4,17 +4,28 @@ from hypothesis import strategies
 
 from tests.utils import MAX_VALUE
 
-MIN_POSITIVE_FLOAT = sys.float_info.min * float(MAX_VALUE)
-assert MIN_POSITIVE_FLOAT < 1.0, MIN_POSITIVE_FLOAT
+
+def to_min_positive_float() -> float:
+    result = 0.5
+    while result * result != 0.0 and result > sys.float_info.min:
+        result /= 2.
+    return result * 2.
+
+
+MIN_POSITIVE_FLOAT = to_min_positive_float()
+assert MIN_POSITIVE_FLOAT < 1., MIN_POSITIVE_FLOAT
 finite_floats = (strategies.floats(-float(MAX_VALUE), -MIN_POSITIVE_FLOAT)
-                 | strategies.just(0.0)
+                 | strategies.just(0.)
                  | strategies.floats(MIN_POSITIVE_FLOAT, float(MAX_VALUE)))
-floats_quadruplets = strategies.lists(finite_floats,
-                                      min_size=4,
-                                      max_size=4)
-floats_sextuplets = strategies.lists(finite_floats,
-                                     min_size=6,
-                                     max_size=6)
-floats_octuplets = strategies.lists(finite_floats,
-                                    min_size=8,
-                                    max_size=8)
+floats_quadruplets = (strategies.lists(finite_floats,
+                                       min_size=4,
+                                       max_size=4)
+                      .map(tuple))
+floats_sextuplets = (strategies.lists(finite_floats,
+                                      min_size=6,
+                                      max_size=6)
+                     .map(tuple))
+floats_octuplets = (strategies.lists(finite_floats,
+                                     min_size=8,
+                                     max_size=8)
+                    .map(tuple))
