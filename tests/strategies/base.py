c97ecce7b02c04d60cf7c63b99c3e2c14247577a
Azat Ibrakov <azatibrakov@gmail.com>
2023-01-05 03:10:10 +0100

    Fix strategies

--- tests/strategies/base.py
+++ tests/strategies/base.py
@@ -7,7 +7,7 @@ from tests.utils import MAX_VALUE
 
 def to_min_positive_float() -> float:
     result = 0.5
-    while result * result != 0.0 and result > sys.float_info.min:
+    while result * result * result != 0.0 and result > sys.float_info.min:
         result /= 2.
     return result * 2.
 
