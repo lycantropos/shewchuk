9e03e6269a2b0848d7e9b369d60f1584336788f6
Azat Ibrakov <azatibrakov@gmail.com>
2023-05-11 01:49:01 +0200

    Complete metadata

--- setup.py
+++ setup.py
@@ -1,43 +1,13 @@
 import platform
-from pathlib import Path
 
 from setuptools import (find_packages,
                         setup)
 
-import shewchuk
-
 project_base_url = 'https://github.com/lycantropos/shewchuk/'
-
-
-def read_file(path_string: str) -> str:
-    return Path(path_string).read_text(encoding='utf-8')
-
-
-parameters = dict(
-        name=shewchuk.__name__,
-        packages=find_packages(exclude=('tests', 'tests.*')),
-        package_data={shewchuk.__name__: ['py.typed']},
-        version=shewchuk.__version__,
-        description=shewchuk.__doc__,
-        long_description=read_file('README.md'),
-        long_description_content_type='text/markdown',
-        author='Azat Ibrakov',
-        author_email='azatibrakov@gmail.com',
-        license='MIT License',
-        classifiers=[
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.11',
-            'Programming Language :: Python :: Implementation :: CPython',
-            'Programming Language :: Python :: Implementation :: PyPy',
-        ],
-        url=project_base_url,
-        download_url=project_base_url + 'archive/master.zip',
-        python_requires='>=3.7'
-)
+parameters = dict(packages=find_packages(exclude=('tests', 'tests.*')),
+                  package_data={'shewchuk': ['__init__.pyi', 'py.typed']},
+                  url=project_base_url,
+                  download_url=project_base_url + 'archive/master.zip')
 if platform.python_implementation() == 'CPython':
     from glob import glob
 
@@ -60,10 +30,7 @@ if platform.python_implementation() == 'CPython':
 
     parameters.update(
             cmdclass={build_ext.__name__: BuildExt},
-            ext_modules=[
-                Extension(f'{shewchuk.__name__}._c{shewchuk.__name__}',
-                          glob('src/*.c'))
-            ],
+            ext_modules=[Extension('shewchuk._cshewchuk', glob('src/*.c'))],
             zip_safe=False
     )
 setup(**parameters)
