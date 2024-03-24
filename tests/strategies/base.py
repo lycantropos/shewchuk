3c77c9d3f078fe5df16d2e17b52843539a570e8d
Azat Ibrakov <azatibrakov@gmail.com>
2024-03-24 17:00:27 +0100

    Fix strategies

--- tests/strategies/base.py
+++ tests/strategies/base.py
@@ -1,26 +1,8 @@
-import sys
-
 from hypothesis import strategies
 
-from shewchuk import incircle_test
+from tests.utils import MAX_VALUE
 
-from tests.utils import MAX_VALUE, exact_incircle_test
-
-
-def to_min_positive_float() -> float:
-    result = 0.5
-    while incircle_test(
-        0.0, 0.0, result, 0.0, 0.0, result, -result, -result
-    ) == exact_incircle_test(
-        0.0, 0.0, result, 0.0, 0.0, result, -result, -result
-    ):
-        result /= 2.0
-        assert result > sys.float_info.min, result
-    return result * 2.0
-
-
-MIN_POSITIVE_FLOAT = to_min_positive_float()
-assert MIN_POSITIVE_FLOAT < 1.0, MIN_POSITIVE_FLOAT
+MIN_POSITIVE_FLOAT = 1.0 / MAX_VALUE
 finite_floats = (
     strategies.floats(-float(MAX_VALUE), -MIN_POSITIVE_FLOAT)
     | strategies.just(0.0)
